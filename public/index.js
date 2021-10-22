612b3271cc3aa2fcc80ccf542040447cfee203a3
Andrew Gordon <andgordo@ucsc.edu>
2021-10-21 19:23:29 -0700

    Refactored home page dictionary index.js, improved project link displays

--- public/index.js
+++ public/index.js
@@ -4,42 +4,42 @@ const projects = {
         'image':'images/delish_food_finder.png', 
         'title': 'images/titles/DELISH_TITLE.png', 
         'subtitle':'A crowdsourced food finding web app aiming to help you find the best ethnic food in your area.',
-        'github_link':'https://github.com/cyruskarsan/Delish-Food',
-        'tech':['GCP', 'Google Maps API', 'JavaScript', 'Python', 'Google Firestore']},
+        'tech':['GCP', 'Google Maps API', 'JavaScript', 'Python', 'Google Firestore'],
+        'project_link':'Project Repository*https://github.com/cyruskarsan/Delish-Food'}, // Project link contains link title and actual link (split by *)
     'GoGuard': {
         'image':'images/meshnetwork.jpg', 
         'title': 'images/titles/GOGUARD_TITLE.png', 
         'subtitle':'A sponsored project by Cisco which creates a mesh network for services that encrypts and dynamically routes communication.',
-        'github_link':'https://csspp.soe.ucsc.edu/2021cisco',
-        'tech':['WireGuard', 'Consul', 'Redis', 'Golang', 'BGP']},
+        'tech':['WireGuard', 'Consul', 'Redis', 'Golang', 'BGP'],
+        'project_link':'Project Summary and Insights*https://csspp.soe.ucsc.edu/2021cisco'},
     'Poly NS Tuner': {
         'image':'images/port1.jpg', 
         'title': 'images/titles/POLYNSTUNER_TITLE.png', 
         'subtitle':'Polyphonic Pitch Detection, Scale Detection and Pitch Tracking Tools for audio with Python.',
-        'github_link':'https://github.com/andrew-d-gordon/portfolio/tree/master/poly-ns-tuner',
-        'tech':['Python', 'Librosa', 'NumPy', 'DSP', 'Fourier Transforms']},
+        'tech':['Python', 'Librosa', 'NumPy', 'DSP', 'Fourier Transforms'],
+        'project_link':'Project Repository*https://github.com/andrew-d-gordon/Poly-NS-Tuner'},
     'Threaded HTTP Server and Load Balancer': {
         'image':'images/port4.jpg', 'title': 
         'images/titles/MTHTTPSERVER_TITLE.png', 
-        'subtitle':'Multi-threaded HTTP Server and Load Balancer which can handle GET, PUT, HEAD and Health Check requests.',
-        'github_link':'https://github.com/andrew-d-gordon/portfolio/tree/master/HTTP_Server_Load_Balancer',
-        'tech':['Objective-C', 'Posix Threads', 'Sockets', 'HTTP', 'Unix']},
+        'subtitle':'Multi-threaded HTTP Server and Load Balancer in C which can handle GET, PUT, HEAD and Health Check requests.',
+        'tech':['Objective-C', 'Posix Threads', 'Sockets', 'HTTP', 'Unix'],
+        'project_link':'Project Repository*https://github.com/andrew-d-gordon/portfolio/tree/master/HTTP_Server_Load_Balancer'},
     'Line Find': {
         'image':'images/linefind.jpg', 
         'title': 'images/titles/LINEFIND_TITLE.png', 
         'subtitle':'A math library in Python created for finding unique sets of lines passing through N amount of 2D Points.',
-        'github_link':'https://github.com/andrew-d-gordon/coding-challenges/tree/main/line-set',
-        'tech':['Python', 'OOP', 'Unittest', 'IO', 'DFS']},
+        'tech':['Python', 'OOP', 'Unittest', 'IO', 'DFS'],
+        'project_link':'Project Repository*https://github.com/andrew-d-gordon/Line-Find'},
     'Image Web Scraper and Icon Generator':  {
         'image':'images/port5.jpg', 
         'title': 'images/titles/ICONGEN_TITLE.png', 
         'subtitle':'An image webscraper and icon generator library for Python.',
-        'github_link':'https://github.com/andrew-d-gordon/portfolio/tree/master/Image_Web_Scraper',
-        'tech':['Python', 'Selenium', 'Pillow', 'WebDriver', 'HTTP']},
+        'tech':['Python', 'Selenium', 'Pillow', 'WebDriver', 'HTTP'],
+        'project_link':'Project Repository*https://github.com/andrew-d-gordon/portfolio/tree/master/Image_Web_Scraper'},
 }
 
 function generateProjectTiles() {
-    project_titles = Object.keys(projects);
+    const project_titles = Object.keys(projects);
     const portfolio_items_wrapper = document.getElementById("items-wrapper");
 
     for (const project_title of project_titles) {
@@ -71,9 +71,9 @@ function generateProjectTiles() {
         let portfolio_tile_subtitle = document.createElement("div");
         portfolio_tile_subtitle.className = "subtitle";
         //Set necessary text attributes (projects may or may not have a repo link)
-        portfolio_tile_projectlink = projects[project_title]['github_link'][0] == 'h' ? projects[project_title]['github_link'] : '#';
-        github_link_text = projects[project_title]['github_link'][0] == 'h' ? 'Project Repo' : projects[project_title]['github_link'];
-        portfolio_tile_subtitle.innerHTML = projects[project_title]['subtitle']+'</br></br><u>Tech:</u> '+projects[project_title]['tech'].join(', ')+'</br></br>'+`<a href=${portfolio_tile_projectlink}>${github_link_text}</a>`;
+        let project_text_link = projects[project_title]['project_link'].split('*');
+        console.log(project_text_link);
+        portfolio_tile_subtitle.innerHTML = projects[project_title]['subtitle']+'</br></br><u>Tech:</u> '+projects[project_title]['tech'].join(', ')+'</br></br>'+`<a href=${project_text_link[1]} target="_blank" rel="noopener noreferrer">${project_text_link[0]}</a>`;
         portfolio_tile_content.appendChild(portfolio_tile_subtitle);
         
         portfolio_tile.appendChild(portfolio_tile_content);
